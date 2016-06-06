e812bc08e0affbbdbb6d3547cdb3753a49220c8e
slevu <s.levu@laposte.net>
2016-06-06 23:10:56 +0100

    extract from dplyr df

--- test.R
+++ test.R
@@ -17,7 +17,8 @@ third_party_gap <- GAP_URL %>%
 
 gs <- gs_title("Copy of Euro 2016 DIDE")
 # gs_browse(gs)
-tab <- gs_read(gs)
+tab <- gs_read(gs, range = cell_rows(1:37))
+?gs_read
 str(tab)
 head(tab)
 names(tab)
@@ -26,5 +27,14 @@ scores <- as.vector(tab[,4])
 Joe <- as.vector(tab[,5])
 ##- compare scores vs Joe
 ?split
-a <- lapply(scores, function(x) strsplit(x, "-"))
-a <- lapply(a[[1]], as.numeric )
+class(a)
+class(scores)
+a <- unlist(apply(scores,2, function(x) strsplit(x, "-")))
+a <- apply(a[[1]],1, as.numeric )
+matrix(as.numeric(a), ncol = 2)
+class(sapply(tab[,4], function(x) strsplit(x, "-")) )
+
+##- extract vector
+tab %>% select(Stephane) %>% collect %>% .[["Stephane"]]
+##- or
+collect(select(tab, Stephane))[[1]]
